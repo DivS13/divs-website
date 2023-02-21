fbf3b59b5d98fb3087eb3ab9c24bdcf467c2d81e
DivS <37397646+DivS13@users.noreply.github.com>
2023-02-21 06:28:25 +0000

    Added dynamic data and api

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+jobs = [{
+  'id': 1,
+  'title': 'Software Engineer',
+  'location': 'Banglore, India',
+  'salary': '$80,000'
+}, {
+  'id': 2,
+  'title': 'Backend Developer',
+  'location': 'Mumbai, India',
+  'salary': '$70,000'
+}, {
+  'id': 3,
+  'title': 'Data Scientist',
+  'location': 'tokyo, Japan',
+  'salary': '$50,000'
+}, {
+  'id': 4,
+  'title': 'Game Developer',
+  'location': 'Gurugram, India',
+  'salary': '$40,000'
+}]
+
+
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=jobs, company_name="DIV")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(jobs)
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
